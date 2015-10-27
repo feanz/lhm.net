6a3f260424eaa2fcb92056ec7d1fdb7e77fdc87f
Richard Forrest <richard.a.forrest@gmail.com>
2015-10-27 10:44:19 +0000

    Add clean up to test script

--- src/lhm.net/Lhm.Test.sql
+++ src/lhm.net/Lhm.Test.sql
@@ -1,3 +1,20 @@
+--Drop all archive tables
+declare @cmd varchar(4000)
+declare cmds cursor for 
+select 'drop table [' + Table_Name + ']'
+from INFORMATION_SCHEMA.TABLES
+where Table_Name like '%_lhm_%'
+
+open cmds
+while 1=1
+begin
+    fetch cmds into @cmd
+    if @@fetch_status != 0 break
+    exec(@cmd)
+end
+close cmds;
+deallocate cmds
+
 IF EXISTS(SELECT 1 FROM sys.tables WHERE object_id = OBJECT_ID('User_Lhm'))
 BEGIN;
     DROP TABLE [User_Lhm];
