997c6c90ed957787e8581c1f1bf138ecd72fd763
Richard Forrest <richard.a.forrest@gmail.com>
2015-10-28 12:15:21 +0000

    Sample console app now runs the DB setup
    * The console app lhm.test is renames to lhm.net.sample (this will let
      us have an lhm.net.test with actual tests
    * Change the name of the sample sql script and removed 'go' so it can be
     executed
    * Run sampledatabase file at start of console app

---
+++ src/lhm.net.sample/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Reflection;
+using Dapper;
+using lhm.net;
+
+namespace lhm.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=(localdb)\\v11.0;;Initial Catalog=Lhm.Test;Integrated Security=True";
+
+            SetupSampleDatabase(connectionString);
+
+            Lhm.Setup(connectionString);
+
+            Lhm.ChangeTable("User", migrator =>
+            {
+                migrator.AddColumn("IsSuspended", "bit");
+            });
+
+            Lhm.ChangeTable("User", migrator =>
+            {
+                migrator.AddColumn("DateOfBirth", "DateTime2");
+            });
+
+            Lhm.ChangeTable("User", migrator =>
+            {
+                migrator.RemoveColumn("DateOfBirth");
+            });
+
+            Console.ReadLine();
+        }
+
+        private static void SetupSampleDatabase(string connectionString)
+        {
+            var directoryName = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+            var sql = File.ReadAllText(Path.Combine(directoryName, "SampleDatabase.sql"));
+
+            using (var con = new SqlConnection(connectionString))
+            {
+                con.Execute(sql);
+            }
+        }
+    }
+}
