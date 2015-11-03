10f9e49d15ef543c6bee7b407040ab67c19f970c
Richard Forrest <richard.a.forrest@gmail.com>
2015-11-03 10:16:38 +0000

    Fix foreign key constraint issue
    resolves #38 #44 # 43
    
    This PR started out as just adding in some data generation so we could
    add a custom number of users to the database to test larger migrations. However during development I noticed a bug with the atomic switcher we where leaving FKey pointing at archive tables.
    
    Work done:
    
    * Drop and recreate FKeys during atomic switch
    * Change user table to staff user is sql reserve word and its a pain
    * Add generation for staff table we now default to adding 50000 staff
    records but this can be upped
    * Seperate the database clean up and setup this is to get around SQL
    statement parsing issues.  Often SQL server will complain about your
    script if the tables already exist and look different to your inserts
    statements
    * Fixed another bug where once in a while multiple migrations end up
    with the same time stamp so it now millisecond precision

---
+++ src/lhm.net.sample/User.cs
@@ -0,0 +1,21 @@
+namespace lhm.net.sample
+{
+    public class User
+    {
+        //public int UserID { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Telephone { get; set; }
+
+        public int DepartmentID { get; set; }
+
+        public int PositionID { get; set; }
+    }
+}
---
+++ src/lhm.net/FKeyInfo.cs
@@ -0,0 +1,16 @@
+namespace lhm.net
+{
+    public class FKeyInfo
+    {
+        //todo fix these names at some point
+        public string PKTABLE_NAME;
+
+        public string PKCOLUMN_NAME;
+
+        public string FKTABLE_NAME { get; set; }
+
+        public string FKCOLUMN_NAME;
+
+        public string FK_NAME { get; set; }
+    }
+}
