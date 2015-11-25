1f8fd3630039505bc6e657e9c07c41a3afdd2215
Richard Forrest <richard.a.forrest@gmail.com>
2015-11-25 16:25:51 +0000

    Add integration tests for table parsing
    Add some extra tests :smile:

---
+++ src/tests/lhm.net.tests.integration/Fixtures/Users.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [Users] (
+    [ID] INTEGER NOT NULL IDENTITY(1, 1),
+    [Reference] int NOT NULL,
+    [Username] NVARCHAR(255) NOT NULL,    
+	[Group] NVARCHAR(255) NOT NULL,    
+	[CreatedAt] datetime NOT NULL,
+    [Comment] varchar(20) DEFAULT NULL,
+    [Description] text,
+    PRIMARY KEY ([ID])
+);
+
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Group_Username] ON [dbo].[Users]
+(
+	[GROUP] ASC,
+	[Username] ASC
+);
+
+CREATE NONCLUSTERED INDEX [IX_UsernameCreatedAt] ON [dbo].[Users]
+(
+	[Username] ASC,
+	[CreatedAt] ASC
+);
---
+++ src/tests/lhm.net.tests.integration/TableTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Should.Fluent;
+using Xunit;
+
+namespace lhm.net.tests.integration
+{
+    public class TableTests 
+    {
+        public class Parser : IntegrationBase
+        {
+            private readonly Table _table;
+
+            public Parser()
+            {
+                _table = CreateTable("users");
+            }
+
+            [Fact]
+            public void Should_parse_table_name()
+            {
+                _table.Name.Should().Equal("users");
+            }
+
+            [Fact]
+            public void Should_parse_primary_key()
+            {
+                _table.PrimaryKey.Should().Equal("ID");
+            }
+
+            [Fact]
+            public void Should_parse_column_types()
+            {
+                _table.Columns.Single(info => info.Name == "Username").DataType
+                    .Should().Equal("nvarchar");
+            }
+
+            [Fact]
+            public void Should_parse_column_meta_data()
+            {
+                _table.Columns.Single(info => info.Name == "Username").IsNullable
+                    .Should().Be.False();
+            }
+        }
+    }
+}
