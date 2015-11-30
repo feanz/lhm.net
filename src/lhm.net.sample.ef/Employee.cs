d588cf38fcd4e306f02d71e571122fbd9adf43bf
Richard Forrest <richard.a.forrest@gmail.com>
2015-11-30 14:16:14 +0000

    Add ef migrations sample application
    Add a sample application that shows how to use Lhm.net in an existing
    entity framework migrations.

---
+++ src/lhm.net.sample.ef/Employee.cs
@@ -0,0 +1,15 @@
+namespace lhm.net.sample.ef
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Grade { get; set; }
+
+        public bool IsSuperuser { get; set; }
+    }
+}
